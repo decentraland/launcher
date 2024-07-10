bc94d97dce643da06df579a66d60ba52231ab992
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-07-10 16:50:10 -0300

    chore: Remove vue references and unused components

--- packages/renderer/src/components/Create/Create.tsx
+++
@@ -1,9 +0,0 @@
-// import {net} from 'electron';
-import React from 'react';
-import {Grid} from 'decentraland-ui2';
-import {AppCard} from '../AppCard/AppCard';
-import {APPS} from '../AppCard/types';
-
-export const Create: React.FC = () => {
-  return <h1>Create</h1>;
-};
