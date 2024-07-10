9557f98e2c29a9a61b465019b312540d6f736cee
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-07-10 16:44:16 -0300

    refactor: Remove router

--- packages/renderer/src/App.tsx
+++ packages/renderer/src/App.tsx
@@ -1,36 +1,20 @@
 import React from 'react';
-import {MemoryRouter as Router, Routes, Route, useNavigate} from 'react-router-dom';
 import {ThemeProvider} from '@mui/material/styles';
-import {Box} from 'decentraland-ui2';
+import {Container} from 'decentraland-ui2';
 import {dark} from 'decentraland-ui2/dist/theme';
-import {Header} from '/@/components/Header/Header';
 import {Home} from '/@/components/Home/Home';
-import {Create} from '/@/components/Create/Create';
-import {Config} from '/@/components/Config/Config';
 
 export const App: React.FC = () => {
   return (
     <React.StrictMode>
       <ThemeProvider theme={dark}>
-        <Box id="app-container">
-          <Router>
-            <Header />
-            <Routes>
-              <Route
-                path="/"
-                element={<Home />}
-              />
-              <Route
-                path="/create"
-                element={<Create />}
-              />
-              <Route
-                path="/config"
-                element={<Config />}
-              />
-            </Routes>
-          </Router>
-        </Box>
+        <Container
+          id="app-container"
+          fixed
+          sx={{display: 'flex', height: '100vh'}}
+        >
+          <Home />
+        </Container>
       </ThemeProvider>
     </React.StrictMode>
   );
