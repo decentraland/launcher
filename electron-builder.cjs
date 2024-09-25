f428987d02aba110e757184f0ae07555536a033b
Juan Cazala <juancazala@gmail.com>
2024-09-25 18:13:31 -0300

    fix: sign windows exe

--- electron-builder.cjs
+++ electron-builder.cjs
@@ -1,3 +1,5 @@
+const { execSync } = require('child_process');
+
 const config = {
   productName: 'Decentraland Launcher',
   artifactName: 'Decentraland Launcher-${os}-${arch}.${ext}',
@@ -87,5 +89,59 @@ const config = {
     },
   ],
 };
+
+// Sign Windows .exe
+if (process.env.CODE_SIGN_SCRIPT_PATH) {
+  config.win.sign = configuration => {
+    console.log('Requested signing for ', configuration.path);
+
+    // Only proceed if the installer .exe file is in the configuration path - skip signing everything else
+    if (!configuration.path.endsWith('Decentraland Launcher-win-x64.exe')) {
+      console.log('This is not the installer .exe, skip signing');
+      return true;
+    }
+
+    const scriptPath = process.env.CODE_SIGN_SCRIPT_PATH;
+
+    try {
+      // Execute the sign script synchronously
+      process.env.INPUT_COMMAND = 'sign'; // override the INPUT_COMMAND, it is already set in the "env" of the GitHub Action step, but for some reason it gets overwritten with 'npx electron-builder ...' so we must set it to 'sign'
+      process.env.INPUT_FILE_PATH = configuration.path; // set the file path to the installer .exe
+      const env = {
+        command: process.env.INPUT_COMMAND,
+        username: process.env.INPUT_USERNAME,
+        password: process.env.INPUT_PASSWORD,
+        credential_id: process.env.INPUT_CREDENTIAL_ID,
+        totp_secret: process.env.INPUT_TOTP_SECRET,
+        file_path: process.env.INPUT_FILE_PATH,
+        output_path: process.env.INPUT_OUTPUT_PATH,
+        malware_block: process.env.INPUT_MALWARE_BLOCK,
+        override: process.env.INPUT_OVERRIDE,
+        clean_logs: process.env.INPUT_CLEAN_LOGS,
+        environment_name: process.env.INPUT_ENVIRONMENT_NAME,
+        jvm_max_memory: process.env.INPUT_JVM_MAX_MEMORY,
+      };
+      console.log('env:', JSON.stringify(env, null, 2));
+      const output = execSync(`node "${scriptPath}"`, {
+        env: { ...process.env, ...env },
+      }).toString();
+      console.log(`Script output: ${output}`);
+    } catch (error) {
+      console.error(`Error executing script: ${error.message}`);
+      if (error.stdout) {
+        console.log(`Script stdout: ${error.stdout.toString()}`);
+      }
+      if (error.stderr) {
+        console.error(`Script stderr: ${error.stderr.toString()}`);
+      }
+      return false;
+    }
+
+    return true; // Return true at the end of successful signing
+  };
+
+  // sign only for Windows 10 and above - adjust for your code as needed
+  config.win.signingHashAlgorithms = ['sha256'];
+}
 
 module.exports = config;
