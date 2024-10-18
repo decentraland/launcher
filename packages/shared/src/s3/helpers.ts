f128d29ecae76a89087361413d55322de7217e30
Gabriel Diaz <gabrieldiaz31@gmail.com>
2024-10-18 18:22:35 -0300

    refactor: Use S3 public endpoint

--- packages/shared/src/s3/helpers.ts
+++ packages/shared/src/s3/helpers.ts
@@ -1,5 +1,5 @@
 export function getBucketURL(): string {
   return import.meta.env.VITE_AWS_ENDPOINT_URL
     ? `${import.meta.env.VITE_AWS_ENDPOINT_URL}/${import.meta.env.VITE_AWS_S3_BUCKET}`
-    : `https://${import.meta.env.VITE_AWS_S3_BUCKET}.s3.amazonaws.com`;
+    : import.meta.env.VITE_AWS_S3_BUCKET_PUBLIC_URL;
 }
