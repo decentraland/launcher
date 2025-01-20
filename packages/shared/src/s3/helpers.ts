fced281e1dc6882e5ef7e0f98c065d0f635b02f6
Gabriel Díaz <git@subsme.slmail.me>
2025-01-20 15:20:03 -0300

    fix: Remove S3 Lib (#94)

--- packages/shared/src/s3/helpers.ts
+++ packages/shared/src/s3/helpers.ts
@@ -1,5 +1,3 @@
 export function getBucketURL(): string {
-  return import.meta.env.VITE_AWS_ENDPOINT_URL
-    ? `${import.meta.env.VITE_AWS_ENDPOINT_URL}/${import.meta.env.VITE_AWS_S3_BUCKET}`
-    : import.meta.env.VITE_AWS_S3_BUCKET_PUBLIC_URL;
+  return import.meta.env.VITE_AWS_S3_BUCKET_PUBLIC_URL;
 }
