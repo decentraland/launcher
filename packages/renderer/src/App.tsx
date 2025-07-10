9cbfbc1470a1e489e1f55da43a4d4c6ebb4aed49
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-07-10 16:32:23 -0300

    fix: Use DCL ThemeProvider

--- packages/renderer/src/App.tsx
+++ packages/renderer/src/App.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router';
-import { ThemeProvider } from '@mui/material/styles';
-import { dark } from 'decentraland-ui2/dist/theme';
-import { CssBaseline } from 'decentraland-ui2';
+import { ThemeProvider, dark } from 'decentraland-ui2/dist/theme';
 import { AppRoutes } from './Routes';
 
 export const App: React.FC = () => {
   return (
     <React.StrictMode>
       <ThemeProvider theme={dark}>
-        <CssBaseline />
         <MemoryRouter>
           <AppRoutes />
         </MemoryRouter>
