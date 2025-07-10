315f58fd3d05cdedc71b2dc3e800b3b9f9a87a63
Gabriel Diaz <gabrieldiaz31@gmail.com>
2025-07-10 16:34:47 -0300

    feat: Add Screen to download the new launcher

---
+++ packages/renderer/src/components/UpdateLauncher/UpdateLauncher.styles.ts
@@ -0,0 +1,11 @@
+import { styled } from 'decentraland-ui2';
+
+export const Container = styled('div')({
+  display: 'flex',
+  height: '100vh',
+  width: '100vw',
+  justifyContent: 'center',
+  alignItems: 'center',
+  background:
+    'linear-gradient(150deg, rgba(165, 36, 179, 1) 0%, rgba(45, 16, 59, 1) 38%, rgba(67, 3, 97, 1) 70%, rgba(165, 36, 179, 1) 100%)',
+});
